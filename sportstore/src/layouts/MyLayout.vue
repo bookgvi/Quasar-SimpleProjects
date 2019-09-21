baec1a14b0106e0e08a76bf5359cabba698cb50d
book <bookgvi@gmail.com>
2019-09-21 19:29:59 +0300

    Prepared Quasar bundle

---
+++ sportstore/src/layouts/MyLayout.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  q-layout
+    q-page-container
+      router-view
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
