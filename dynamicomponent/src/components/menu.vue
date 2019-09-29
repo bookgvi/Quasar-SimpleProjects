744bafb9b48806b738cf5fb980bbce534fe6cec4
book <bookgvi@gmail.com>
2019-09-29 15:15:54 +0300

    style for top menu

--- dynamicomponent/src/components/menu.vue
+++ dynamicomponent/src/components/menu.vue
@@ -1,16 +1,15 @@
 <template lang="pug">
-  .navbar
-    q-page-sticky(expand position="top")
-    q-tabs
-      q-route-tab(to="dynamic")
-        .inline-block.text-uppercase dynamic
-      q-route-tab(to="first")
-        .inline-block.text-uppercase first
-      q-route-tab(to="second")
-        .inline-block.text-uppercase second
-      q-route-tab(to="third")
-        .inline-block.text-uppercase third
-    q-separator
+  .topmenu.q-py-xl
+    q-page-sticky(position="top" style="border-bottom: 1px solid silver; z-index: 10000")
+      q-tabs
+        q-route-tab(to="dynamic")
+          .inline-block.text-uppercase dynamic
+        q-route-tab(to="first")
+          .inline-block.text-uppercase first
+        q-route-tab(to="second")
+          .inline-block.text-uppercase second
+        q-route-tab(to="third")
+          .inline-block.text-uppercase third
 </template>
 
 <script>
