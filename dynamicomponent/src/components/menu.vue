e2fa293d6fdb36b0e680132119796341df4397ac
book <bookgvi@gmail.com>
2019-09-29 12:34:23 +0300

    Menu

---
+++ dynamicomponent/src/components/menu.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  q-toolbar
+    q-toolbar-title
+      .inline-block QQQ
+      .inline-block WWW
+      .inline-block EEE
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {}
+  }
+}
+</script>
--- dynamicomponent/src/layouts/MyLayout.vue
+++ dynamicomponent/src/layouts/MyLayout.vue
@@ -1,5 +1,13 @@
 <template lang="pug">
   q-layout(view="lHh Lpr lFf")
     q-page-container
+      TopMenu
       router-view
 </template>
+
+<script>
+import TopMenu from '../components/menu'
+export default {
+  components: { TopMenu }
+}
+</script>
