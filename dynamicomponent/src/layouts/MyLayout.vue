f99dc45ee5e82e9e5c45462dbd226f748b640c2f
book <bookgvi@gmail.com>
2019-09-29 14:03:50 +0300

    Styles

--- dynamicomponent/src/layouts/MyLayout.vue
+++ dynamicomponent/src/layouts/MyLayout.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   q-layout(view="lHh Lpr lFf")
-    q-page-container.q-pt-md
-      TopMenu
-      .row.q-pt-md.justify-between
-        .com-9.q-px-xl
+    q-page-container.q-pt-md.q-px-xl
+      TopMenu.q-pb-lg
+      .row.justify-center
+        .col-9
           router-view
 </template>
 
