5ffc7b7fb8967cfa1b2dae715a92c69a12d1e174
book <bookgvi@gmail.com>
2019-09-29 11:54:24 +0300

    starting preparation

---
+++ dynamicomponent/src/layouts/MyLayout.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  q-layout(view="lHh Lpr lFf")
+    q-page-container
+      router-view
+</template>
---
+++ dynamicomponent/src/pages/Index.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  q-page
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
