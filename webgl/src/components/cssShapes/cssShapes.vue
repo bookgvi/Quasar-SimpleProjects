a65517d7b0d6fda8255cdf4d5405adbfa649897e
book <bookgvi@gmail.com>
2019-10-05 19:13:30 +0300

    WebGl: some shapes for cssShapes page

--- webgl/src/components/cssShapes/cssShapes.vue
+++ webgl/src/components/cssShapes/cssShapes.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
-  .row.q-ma-lg
-    div.emptysquare
+  .cssShapes
+    .row.q-ma-lg.justify-center
+      .text-h4 CSS Shapes
+    .row.q-ma-lg.justify-center
+      div.q-mr-sm.emptysquare
+      div.q-mr-sm.triangle
+      div.circle
 </template>
 
 <script>
@@ -19,5 +24,18 @@ export default {
     border-right: 50px solid #777777;
     border-bottom: 50px solid #777777;
     background: #ffffff;
+  }
+  .triangle {
+    width: 0;
+    height: 0;
+    border-left: 50px solid transparent;
+    border-right: 50px solid transparent;
+    border-bottom: 100px solid #777;
+  }
+  .circle {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background: #777;
   }
 </style>
