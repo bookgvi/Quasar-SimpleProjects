95db3b83179f4548d0a8b0370f21518ee6e35e82
book <bookgvi@gmail.com>
2019-10-05 17:42:05 +0300

    WebGl: vueLogo component - realised with css

--- webgl/src/components/cssShapes/cssShapes.vue
+++ webgl/src/components/cssShapes/cssShapes.vue
@@ -1,8 +1,6 @@
 <template lang="pug">
-  .row
-    div.q-ma-lg.square
-    div.q-ma-lg.circle
-    div.q-ma-lg.triangle
+  .row.q-ma-lg
+    div.emptysquare
 </template>
 
 <script>
@@ -14,22 +12,11 @@ export default {
 </script>
 
 <style scoped>
-  .square {
+  .emptysquare {
     width: 100px;
     height: 100px;
-    background: #777;
-  }
-  .circle {
-    height: 100px;
-    width: 100px;
-    background: #777;
-    border-radius: 50%;
-  }
-  .triangle {
-    width: 0px;
-    height: 0px;
-    border-left: 50px solid transparent;
-    border-right: 50px solid transparent;
-    border-bottom: 100px solid #777;
+    border-left: 50px solid #777777;
+    border-right: 50px solid #777777;
+    border-bottom: 100px solid #777777;
   }
 </style>
---
+++ webgl/src/components/cssShapes/vueLogo.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  .row.q-ma-lg
+    div.triangle1
+    div.triangle2
+    div.triangle3
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+  .triangle1 {
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-right: 50px solid transparent;
+    border-left: 50px solid transparent;
+    border-top: 96px solid #407753;
+    z-index: 1000;
+  }
+  .triangle2 {
+    position: absolute;
+    left: 48px;
+    width: 0;
+    height: 0;
+    border-right: 27px solid transparent;
+    border-left: 27px solid transparent;
+    border-top: 52px solid #430077;
+    z-index: 1001;
+  }
+  .triangle3 {
+    position: absolute;
+    left: 63px;
+    width: 0;
+    height: 0;
+    border-right: 12px solid transparent;
+    border-left: 12px solid transparent;
+    border-top: 25px solid #fff;
+    z-index: 1002;
+  }
+  .emptysquare {
+    width: 100px;
+    height: 100px;
+    border-left: 50px solid #777777;
+    border-right: 50px solid #777777;
+    border-bottom: 100px solid #777777;
+  }
+</style>
