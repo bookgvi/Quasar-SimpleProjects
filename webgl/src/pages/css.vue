4f83bd53685c8e029752be6b1c2712055624d446
book <bookgvi@gmail.com>
2019-10-05 17:06:30 +0300

    WebGl: routing

---
+++ webgl/src/pages/css.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  q-page
+    cssShape
+</template>
+
+<script>
+import cssShape from '../components/cssShapes/cssShapes'
+export default {
+  name: 'triangles',
+  components: {
+    cssShape
+  }
+}
+</script>
