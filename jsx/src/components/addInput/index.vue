ea50388a38252254cf26f0ddbce4036729412bc8
book <bookgvi@gmail.com>
2019-10-16 00:22:35 +0300

    JSX: dataset-number для идентификации новосозданных инпутов

--- jsx/src/components/addInput/index.vue
+++ jsx/src/components/addInput/index.vue
@@ -2,6 +2,7 @@
 export default {
   name: 'index',
   data: () => ({
+    inputCount: -1,
     input1: 'QQQ'
   }),
   render (createElement) {
@@ -59,6 +60,8 @@ export default {
       const inputBlock = document.querySelector('.inputBlock')
       const newInput = document.createElement('input')
       const appendInput = inputBlock.appendChild(newInput)
+      this.inputCount++
+      appendInput.dataset.number = this.inputCount
       appendInput.style.width = '100%'
       appendInput.style.height = '2.5rem'
       appendInput.classList.add('q-mt-md')
