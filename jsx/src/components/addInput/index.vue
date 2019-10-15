5b06d98d62074f59b1d10a59cdd613bf35f07bf0
book <bookgvi@gmail.com>
2019-10-15 23:48:21 +0300

    JSX: render function

--- jsx/src/components/addInput/index.vue
+++ jsx/src/components/addInput/index.vue
@@ -1,18 +1,58 @@
-<template lang="pug">
-  .input.row.q-pt-lg.justify-center
-    .col-9
-      .row
-        .input.col.q-pr-sm
-          q-input(v-model="input1" outlined dense)
-        .col
-          q-btn.bg-grey.text-white(label="Add field")
-</template>
-
 <script>
 export default {
   name: 'index',
   data: () => ({
-    input1: ''
-  })
+    input1: 'QQQ'
+  }),
+  render (createElement) {
+    const wrap = createElement('div', {
+      class: {
+        row: true,
+        'no-wrap': true
+      }
+    },
+    [
+      createElement('div', {
+        class: {
+          'inline-block': true,
+          'col-4': true
+        }
+      },
+      [
+        createElement('q-input', {
+          value: this.input1,
+          class: {
+            'q-field--outlined': true,
+            'q-field--dense': true,
+            'q-pr-md': true
+          },
+          on: {
+            change: this.inputChange
+          }
+        })
+      ]),
+      createElement('div', {
+        class: {
+          'col-4': true
+        }
+      },
+      [
+        createElement('q-btn', {
+          on: {
+            click: this.addInput
+          }
+        }, 'Click me')
+      ])
+    ])
+    return createElement('div', [wrap])
+  },
+  methods: {
+    addInput () {
+      console.log('btn clicked!')
+    },
+    inputChange (value) {
+      console.log('Inside inputChange method', value)
+    }
+  }
 }
 </script>
