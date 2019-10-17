bdf9b77d6911838bd4311f64716fc35f164a0ea0
book <bookgvi@gmail.com>
2019-10-18 01:46:14 +0300

    PUT

--- validatephone/src/components/Form/form.vue
+++ validatephone/src/components/Form/form.vue
@@ -5,14 +5,33 @@
         q-input(
           v-if="!isPhoneInput"
           style="width: 150%;"
+          placeholder="+7 (900) 800 1234"
           outlined
           dense
         )
         q-input(
           v-if="isPhoneInput"
           :value="reformatPhoneNumber(firstStudio.phone) | phoneFormat"
-          @change.native="hInput"
+          @change.native="hPhone"
           placeholder="+7 (900) 800 1234"
+          style="width: 150%;"
+          outlined
+          dense
+        )
+    .row.q-pb-md
+      .col
+        q-input(
+          v-if="!isPhoneInput"
+          style="width: 150%;"
+          placeholder="email@domain.com"
+          outlined
+          dense
+        )
+        q-input(
+          v-if="isPhoneInput"
+          :value="firstStudio.email"
+          @change.native="hEmail"
+          placeholder="email@domain.com"
           style="width: 150%;"
           outlined
           dense
@@ -32,10 +51,14 @@ export default {
     }
   },
   methods: {
-    hInput (e) {
+    hPhone (e) {
       let value = e.target.value
       value = this.reformatPhoneNumber(value)
-      this.$emit('hInput', value)
+      this.$emit('hPhone', value)
+    },
+    hEmail (e) {
+      const value = e.target.value
+      this.$emit('hEmail', value)
     },
     reformatPhoneNumber (phone) {
       return String(phone.split('').filter(item => !isNaN(item) && item !== ' ').join(''))
--- validatephone/src/components/Form/index.vue
+++ validatephone/src/components/Form/index.vue
@@ -5,10 +5,13 @@
         form-component(
           :firstStudio="firstStudio"
           :isPhoneInput="isPhoneInput"
-          @hInput="hInput"
+          @hPhone="hPhone"
+          @hEmail="hEmail"
         )
+      .row.q-pb-sm
+        q-btn.bg-primary.text-white(label="Get data" @click="getStudios" outlined dense no-caps style="width: 50%;")
       .row
-        q-btn.bg-primary.text-white(label="Get phone number" @click="getStudios" outlined dense no-caps style="width: 50%;")
+        q-btn.bg-grey.text-white(label="Set new data" @click="setStudios" outlined dense no-caps style="width: 50%;")
 
 </template>
 
@@ -17,19 +20,27 @@ import FormComponent from './form'
 export default {
   components: { FormComponent },
   data: () => ({
+    relURL: 'cabinet/v1.0/',
     firstStudio: {},
-    isPhoneInput: false
+    isPhoneInput: false,
+    studioID: 0
   }),
   methods: {
-    hInput (value) {
-      console.log(value)
-      this.tel = value
+    hPhone (value) {
+      this.firstStudio.phone = value
+    },
+    hEmail (value) {
+      this.firstStudio.email = value
     },
     async getStudios () {
-      const relURL = 'cabinet/v1.0/'
-      const { items } = await this.$http.get(`${relURL}studios`).then(resp => resp.data.data)
+      const { items } = await this.$http.get(`${this.relURL}studios`).then(resp => resp.data.data)
       this.firstStudio = items[0]
+      this.studioID = items[0].id
       this.isPhoneInput = true
+    },
+    async setStudios () {
+      const result = await this.$http.put(`${this.relURL}studios/${this.studioID}`, this.firstStudio).then(resp => resp.data)
+      console.log(result)
     }
   },
   async created () {
